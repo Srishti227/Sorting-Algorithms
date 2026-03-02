62848a4cfcf25bc8e87872b5488d3a48c86c35c5
Srishti227 <srishtimahajanshetti@gmail.com>
2026-03-02 16:31:17 +0530

    v2.1: added user input support
    Modified main function to accept user input for array size and elements.

--- Selectionsort.c
+++ Selectionsort.c
@@ -26,8 +26,13 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = 5;
+    int n;
+    printf("Enter the no of elements: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements\n");
+    for(int i=0;i<n;i++)
+        scanf("%d",a[i]);
     selectionSort(arr, n);
 
     printf("Sorted array: \n");
