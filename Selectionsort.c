d81d322e0470310c791cb36e9555868685f93e23
Srishti227 <srishtimahajanshetti@gmail.com>
2026-03-02 16:15:30 +0530

    v1.0: basic selection sort implementation

---
+++ Selectionsort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Function to perform selection sort
+void selectionSort(int arr[], int n) {
+    int i, j, min, temp;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        if (min!= i) {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = 5;
+    selectionSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
